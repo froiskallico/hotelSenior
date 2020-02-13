67257c835f025bc1a04903c9bfbc843efa3c9b29
KallicoFrois <froiskallico@gmail.com>
2020-02-13 15:45:57 -0300

    feat: GuestController.show method finalized

--- backend/src/controllers/GuestController.js
+++ backend/src/controllers/GuestController.js
@@ -4,32 +4,56 @@ module.exports = {
   async store(req, res) {
     const { nome, documento, telefone } = req.body;
 
-    var guest = await Guest.findOne({ documento });
+    let guest = await Guest.findOne({ documento });
 
     try {
       if (!guest) {
         guest = await Guest.create({
           nome,
           documento,
-          telefone
+          telefone,
         });
 
-        return res.json(guest);
+        return res.json({ guest });
       }
       return res
         .status(409)
         .json({
-          error: 'Hóspede já cadastrado',
-          guest
-      });
+          error: 'Hóspede já cadastrado com este numero de Documento',
+          guest,
+        });
     } catch (err) {
       return res.status(500);
     }
   },
 
   async show(req, res) {
-    const { nome, documento, telefone } = req.query;
+    const {
+      nome,
+      documento,
+      telefone,
+      pg,
+      pg_size,
+    } = req.query;
 
-    console.log(nome, documento, telefone);
-  }
+    const guest = await Guest
+      .find({
+        nome: new RegExp(nome, 'i'),
+        documento: new RegExp(documento, 'i'),
+        telefone: new RegExp(telefone, 'i'),
+      })
+      .limit(parseInt(pg_size) || 10)
+      .skip(parseInt(pg_size) * (parseInt(pg) || 0));
+
+    res.json({ guest });
+  },
+
+  // async update(req, res) {
+
+  // },
+
+  // async destroy(req, res) {
+
+  // },
+
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const GuestController = require('./controllers/GuestController');
 const routes = Router();
 
 routes.post('/guests', GuestController.store);
-routes.get('/guests/:nome/:documento/:telefone', GuestController.show);
+routes.get('/guests', GuestController.show);
 // routes.get('/guests', GuestController.index);
 
 module.exports = routes;
