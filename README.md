e9edf3ce0212128f0034435d043c4d8ab4994481
Kallico Fróis <froiskallico@gmail.com>
2020-02-14 18:10:41 -0300

    build: Bill Model created

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Aplicação para Check-in/Check-out de hóspedes em um hotel.wigwip
 
 ## API debug
-[![Run in Insomnia}](https://insomnia.rest/images/run.svg)](https://insomnia.rest/run/?label=hotelSenior&uri=https%3A%2F%2Fwww.github.com%2Ffroiskallico%2FhotelSenior%2master%2Futils%2Finsomnia.json)
+[![Run in Insomnia}](https://insomnia.rest/images/run.svg)](https://insomnia.rest/run/?label=hotelSenior&uri=https://gist.githubusercontent.com/froiskallico/9ffec3e72a77986f02da6e81fb0ece02/raw/444a3f46775734c6c08db06ed0adb80a93d358b0/insomnia)
--- backend/src/models/Bill.js
+++ backend/src/models/Bill.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const GuestSchema = require('Guest');
 
 const BillSchema = new mongoose.Schema({
-  check-in: { type: Date, required: true },
-  check-out: Date,
   hospede: {
     type: GuestSchema,
-    required: true
+    required: true,
   },
-  garagem: { type: Boolean, required: true },
-  valor: Number
+  dataEntrada: { type: Date, required: true },
+  dataSaida: Date,
+  adicionalVeiculo: { type: Boolean, required: true },
+  valor: Number,
 });
 
 module.exports = new mongoose.model('Bill', BillSchema);
